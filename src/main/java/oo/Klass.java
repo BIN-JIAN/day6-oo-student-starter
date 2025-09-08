809594b91b84af9c2bd8fbdfa88c3eb0679468df
BIN <BIN.JIAN@OOCL.COM>
2025-09-08 14:46:23 +0800

    test: add test for case1,2

---
+++ src/main/java/oo/Klass.java
@@ -0,0 +1,20 @@
+package oo;
+
+public class Klass {
+  private int number;
+
+  public Klass(int i) {
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public void setNumber(int number) {
+    this.number = number;
+  }
+
+
+
+
+}
--- src/test/java/oo/step4/TeacherTest.java
+++ src/test/java/oo/step4/TeacherTest.java
@@ -1,10 +1,31 @@
 package oo.step4;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import oo.Klass;
+import oo.Teacher;
+import org.junit.jupiter.api.Test;
+
 public class TeacherTest {
 
     // when belongsTo give teacher does not teach the class then return false
-
+     @Test
+     void should_belongsTo_give_when_not_teach_then_return_false(){
+       Teacher teacher1 = new Teacher(1,"aa",35);
+       Klass klass1 = new Klass(1);
+       Klass klass2 = new Klass(4);
+       teacher1.assignTo(klass2);
+       assertEquals(false,teacher1.belongsTo(klass1));
+     }
     // when belongsTo give teacher teach the class then return true
+     @Test
+     void should_belongsTo_given_when_teach_return_true(){
+       Teacher teacher1 = new Teacher(1,"aa",35);
+       Klass klass1 = new Klass(1);
+       teacher1.assignTo(klass1);
+       assertEquals(true,teacher1.belongsTo(klass1));
+     }
 
     // when belongsTo given teacher teach multiple classes then return true
 
